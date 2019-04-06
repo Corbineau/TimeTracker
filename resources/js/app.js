1830622b9f6852a5d0732e65a16410d97cc93ca0
Rabbit <Corbineau@users.noreply.github.com>
2019-04-06 11:10:05 -0400

    updated js with database info

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,31 @@
-//include the database link here
+//include the database code here
+
+var config = {
+    apiKey: "AIzaSyCHlHCclA3zjaaCRo5Q9tJkqe7t0cWKaL8",
+    authDomain: "timetrackercream.firebaseapp.com",
+    databaseURL: "https://timetrackercream.firebaseio.com",
+    projectId: "timetrackercream",
+    storageBucket: "timetrackercream.appspot.com",
+    messagingSenderId: "584206027961"
+  };
+  firebase.initializeApp(config);
+
+  db = firebase.database();
 
 //need to have the on-click event
 
 $("#").on("click", () => {
-    //get the new employee values from the input fields
+    //get the new employee values from the input fields. Doing these as variables instead of an object because I am in a ton of pain and this is less thinking.
+    let name = $("#name").val().trim();
+    let role = $("#role").val().trim();
+    let startDate = $("#startDate").val().trim();
+    let rate = $("#monthlyRate").val().trim();
     //store them in the database
+    db.ref().push( {
+        name: name,
+        role: role,
+        startDate: startDate,
+        rate: rate
+    })
     //append new info to the employee table
 });
