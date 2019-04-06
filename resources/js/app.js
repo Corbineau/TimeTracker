5759f0132a831d6e7b8d290bd60a6338f707c743
Rabbit <Corbineau@users.noreply.github.com>
2019-04-06 10:48:34 -0400

    adding base .js file

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+//include the database link here
+
+//need to have the on-click event
+
+$("#").on("click", () => {
+    //get the new employee values from the input fields
+    //store them in the database
+    //append new info to the employee table
+});
